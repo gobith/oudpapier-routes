ad1ebef2ad382c9a614c2683c22c5d229c2898ad
Georges <gkgobith@gmail.com>
2025-05-09 13:50:33 +0200

    Add Leaflet map integration and global styles
    - Introduced Leaflet as a dependency in package.json and package-lock.json.
    - Created global styles in app.css for consistent styling.
    - Updated +page.svelte to implement a Leaflet map with a marker.
    - Added +page.ts to disable server-side rendering.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import 'leaflet/dist/leaflet.css';
+	import '../app.css';
+
+	import { onMount } from 'svelte';
+	import L from 'leaflet';
+
+	let mapDiv: HTMLDivElement;
+
+	onMount(() => {
+		const map = L.map(mapDiv).setView([51.505, -0.09], 13);
+
+		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			attribution: '&copy; OpenStreetMap contributors'
+		}).addTo(map);
+
+		L.marker([51.505, -0.09])
+			.addTo(map)
+			.bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+			.openPopup();
+	});
+</script>
+
+<div bind:this={mapDiv}></div>
+
+<style>
+	div {
+        height: 100vh;
+        width: 100%;
+        margin: 0;
+        padding: 0;
+	}
+</style>
