9f5be2f94fd03b906899cc3bf405e68743ae48cb
Georges <gkgobith@gmail.com>
2025-05-09 14:03:44 +0200

    Update map initialization and marker popup in +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,18 +6,26 @@
 	import L from 'leaflet';
 
 	let mapDiv: HTMLDivElement;
+	let lat = $state(0);
+	let lng = $state(0);
 
 	onMount(() => {
-		const map = L.map(mapDiv).setView([51.505, -0.09], 13);
+		const map = L.map(mapDiv).setView([52.43162959873308, 4.911296367645265], 16);
 
 		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 			attribution: '&copy; OpenStreetMap contributors'
 		}).addTo(map);
 
-		L.marker([51.505, -0.09])
+		L.marker([52.43162959873308, 4.911296367645265])
 			.addTo(map)
-			.bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+			.bindPopup('Begin route')
 			.openPopup();
+
+		map.on('click', (e: L.LeafletMouseEvent) => {
+			lat = e.latlng.lat;
+			lng = e.latlng.lng;
+			console.log(`Clicked at: ${lat}, ${lng}`);
+		});
 	});
 </script>
 
@@ -25,9 +33,9 @@
 
 <style>
 	div {
-        height: 100vh;
-        width: 100%;
-        margin: 0;
-        padding: 0;
+		height: 100vh;
+		width: 100%;
+		margin: 0;
+		padding: 0;
 	}
 </style>
